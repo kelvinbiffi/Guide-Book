64d114cdf0839a386cf921624ca9417067ea23e6
kelvin.biffi <kelvin.biffi@pmweb.com.br>
2021-01-20 23:16:42 -0300

    Ajustado para gerar os Objetos de Exemplo baseado nos comentários de exemplos dos arquivos de estilo

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const fs = require('fs');
  */
 class GuideBook {
 
+    constructor () {
+        this.examples = [];
+    }
+
     /**
      * Generate Guide Book
      * 
@@ -30,6 +34,7 @@ class GuideBook {
         if (this.validateSettingsStructure(settings)) {
             const css = this.getStyleCSS(settings.style, settings.charset);
             const examples = this.getExamplesFromSource(settings.source, settings.charset);
+            console.log(this.examples);
         }
     }
 
@@ -69,7 +74,7 @@ class GuideBook {
     getStyleCSS (style, charset) {
         const filePath = path.resolve(style);
         if (!fs.existsSync(filePath)) {
-            throw new Error('Invalid: Built Style File Not Exists');
+            throw new Error('Invalid: Built Style File Do Not Exists');
         }
 
         let css = fs.readFileSync(filePath, charset);
@@ -77,16 +82,95 @@ class GuideBook {
         return css;
     }
 
+    isDirectory (path) {
+        return fs.lstatSync(path).isDirectory();
+    }
+
     /**
-     * Get the examples inside Files Source
+     * Check if is a directory or files and start get the examples
      * 
-     * @param {String} source Source CSS Path
+     * @param {String} source Source Style Path
      * @param {String} charset Charset Default UTF8
      * 
      * @return {Array}
      */
     getExamplesFromSource (source, charset) {
+        const sourcePath = path.resolve(source);
+        if (!fs.existsSync(sourcePath)) {
+            throw new Error('Invalid: Source Folder or File Do Not Exists');
+        }
+        
+        if (this.isDirectory(sourcePath)) {
+            console.log('is a directory');
+            this.iterateSourceFolders(sourcePath, charset);
+        } else {
+            this.getExampleFromSourceFile(sourcePath, charset);
+        }
+    }
 
+
+    iterateSourceFolders (sourcePath, charset) {
+        const filenames = fs.readdirSync(sourcePath); 
+        filenames.forEach(file => {
+            const filePath = sourcePath + '\\' + file;
+            if (this.isDirectory(filePath)) {
+                this.iterateSourceFolders (filePath, charset);
+            } else {
+                this.getExampleFromSourceFile(filePath, charset);
+            }
+        });
+    }
+
+    /**
+     * Get the examples inside Files Source
+     * 
+     * @param {String} sourcePath Source Style File Path
+     * @param {String} charset Charset Default UTF8
+     * 
+     * @return {Array}
+     */
+    getExampleFromSourceFile (sourcePath, charset) {
+        let sourceFile = fs.readFileSync(sourcePath, charset);
+        sourceFile = sourceFile.toString();
+        this.extractExamplesFromFile(sourceFile);
+    }
+
+    extractExamplesFromFile (File) {
+        if (File.indexOf('/*GUIDE') === -1) {
+            return;
+        }
+        let examples = File.split('/*GUIDE');
+        examples = examples
+            .map((i) => (i.indexOf('*/') > -1 ? i.split('*/')[0] : null))
+            .filter((i) => i)
+            .map((e) => this.refineExample(e));
+        this.examples = this.examples.concat(examples);
+    }
+
+    /**
+     * Refine the rough example and convert into a object Example
+     * 
+     * @param {Array} example rough example
+     */
+    refineExample(example) {
+        const exampleInfo = example.split('\r\n')
+        if (exampleInfo[0].trim() === '') {
+            exampleInfo.shift();
+        }
+        if (exampleInfo[exampleInfo.length - 1].trim() === '') {
+            exampleInfo.pop();
+        }
+        const categorySession = exampleInfo[0].split('|');
+        if (categorySession.length !== 2) {
+            throw new Error(`Invalid: Settings ${exampleInfo[0]} do not match pattern CATEGORY|SESSION. See Documentation.`);
+        }
+
+        exampleInfo.shift();
+        return {
+            module: categorySession[0],
+            session: categorySession[1],
+            example: exampleInfo.join('\r\n')
+        };
     }
 }
 
